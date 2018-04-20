370eb0096e1546f40db86f95248cf76baafdc9f0
Luke <25354036+cotton105@users.noreply.github.com>
2018-04-20 09:19:48 +0100

    Add comments to NFCRegister.py
    General documentation to help clarify the data flow in the initial file.

--- NFC Register.py
+++ NFC Register.py
@@ -5,13 +5,14 @@ from Admin import Admin
 from Database import Database
 
 global cwd
-cwd = os.path.dirname(os.path.realpath(__file__))
+cwd = os.path.dirname(os.path.realpath(__file__)) #Set the directory to use to be the current working directory
 
 def GetUserID():
-    UserID = int(input('UserID: '))
+    UserID = int(input('UserID: ')) #User enters a UserID manually (to be changed)
     return UserID
 
 def GetNFCRegisterConnection():
+    #Establish arguments to use for the connection to the database
     dbConnection = mariadb.connect(host='localhost',
                                    user='dbadmin',
                                    passwd='dbadmin',
@@ -20,27 +21,27 @@ def GetNFCRegisterConnection():
     return dbConnection
 
 #Create the database file if it does not already exist
-if not os.path.isdir('/var/lib/mysql/NFCRegister'):
+if not os.path.isdir('/var/lib/mysql/NFCRegister'): #Check if the specified directory exists at this location
     dbConnection = mariadb.connect(host='localhost',
                                    user='dbadmin',
                                    passwd='dbadmin',
                                    port=3306,
                                    db='mysql')
-    db = Database(dbConnection)
-    db.CreateNewDatabase()
-    db = Database(GetNFCRegisterConnection())
-    db.AddToNewDatabase()
+    db = Database(dbConnection) #Create new database object instance
+    db.CreateNewDatabase() #Create the database
+    db = Database(GetNFCRegisterConnection()) #Connect to the newly created database
+    db.AddToNewDatabase() #Add the necessary entities to the database
 
 db = Database(GetNFCRegisterConnection())
 
 UserID = GetUserID()
-while UserID != 0:
-    Authorisation = db.GetAuthorisation(UserID)
+while UserID != 0: #Stop program when 0 is entered as a UserID
+    Authorisation = db.GetAuthorisation(UserID) #Check the UserID's matching Auth
     if Authorisation == 'STUDENT' or Authorisation == 'TEACHER':
         Person = User(UserID, db)
     elif Authorisation == 'ADMIN':
         Person = Admin(UserID, db)
-    Person.Register()
+    Person.Register() #Go through all the logic neccessary to register user with the database
     UserID = GetUserID()
 
-db.dbConnection.close()
+db.dbConnection.close() #Finally close the connection to the database so no corruption occurs
